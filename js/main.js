1f4a30abbcae19b4cf215f3aa329bb73e7fa4c8c
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2016-03-04 13:07:58 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 (function(win, $) {
     $(function() {
+
+        new WOW().init();
+
+        $(".typed-text").typed({
+            strings: ["Kolektivní energie^1000 =", "^300 Levnější plyn^1000<br>a elektřina.^3000"],
+            typeSpeed: 80,
+            contentType: 'html',
+            loop: true
+        });
+
 	    $('a[data-scroll]').on('click', function(e) {
 	        e.stopImmediatePropagation();
 	        var $el = $(this);
