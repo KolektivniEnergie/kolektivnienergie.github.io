6742d12513b5041bd5cf33a851459114d0866118
Jaroslav Urbánek <jaroslav.urbanek@core.cz>
2016-03-14 18:29:04 +0100

    Update page

--- js/main.js
+++ js/main.js
@@ -4,20 +4,24 @@
         new WOW().init();
 
         $(".typed-text").typed({
-            strings: ["Kolektivní energie^1000 =", "^300 Levnější plyn^1000<br>a elektřina.^3000"],
+            strings: ["Kolektivní energie^1000 =", "^300 Levnější plyn^1000,<br>elektřina^1000 a biomasa.^3000"],
             typeSpeed: 80,
             contentType: 'html',
             loop: true
         });
 
 	    $('a[data-scroll]').on('click', function(e) {
-	        e.stopImmediatePropagation();
+	    	e.stopImmediatePropagation();
 	        var $el = $(this);
 	        var scrollTo = $el.data('scroll');
-	        $('body').scrollTo(scrollTo, {
+	        $('html,body').scrollTo(scrollTo, {
 	            duration : '1000',
 	            offsetTop : '30'
 	        });
+
+	        if(scrollTo.indexOf("#") >= 0) {
+	        	location.href = scrollTo;
+	        }
 
 	        return false;
 	    });
